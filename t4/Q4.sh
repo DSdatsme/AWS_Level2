5b14fb964c39fba3bfaeb077d898c1e16d58d984
DSdatsme <darsuratwala@gmail.com>
2018-08-04 14:49:37 +0530

    t4 wip

--- t4/Q4.sh
+++ t4/Q4.sh
@@ -37,10 +37,15 @@ echo "Your start function is created!"
 rm start-func.py tempzip.zip
 
 echo 'Creating start Targets.....'
+echo "[
+  {
+    \"Id\": "1", 
+    \"Arn\": "\""$startFunctionArn"\""
+  }
+]" > target-start.json
 aws events put-targets \
 --rule start-rule \
 --targets file://target-start.json
-rm target-start.json
 
 #lambda stop-func
 echo -e "import boto3\nregion = '${REGION}'\ninstances = ['i-0c88bb86fedfcc299']\ndef lambda_handler(event, context):\n    print(event)\n    ec2 = boto3.client('ec2', region_name=region)\n    ec2.stop_instances(InstanceIds=instances)\n    return 'GoodBye see you soon.....'" > stop-func.py
@@ -57,10 +62,16 @@ echo "Your stop function is created!"
 rm stop-func.py tempzip.zip
 
 echo 'Creating stop Targets...'
+echo "[
+  {
+    \"Id\": "1", 
+    \"Arn\": "\""$stopFunctionArn"\""
+  }
+]" > target-stop.json
 aws events put-targets \
 --rule stop-rule \
 --targets file://target-stop.json
-rm target-stop.json
+
 #lambda reset-func
 echo -e "import boto3\na = ['MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT', 'SUN']\ndef lambda_handler(event, context):\n    client = boto3.client('events')\n    for i in range(0,7):\n        response = client.disable_rule(Name=a[i]+'-start-rule')\n        response = client.disable_rule(Name=a[i]+'-stop-rule')\n    response = client.enable_rule(Name='start-rule' )\n    response = client.enable_rule(Name='stop-rule')\n    return 'Hello from Lambda'" > reset-func.py
 zip tempzip.zip reset-func.py
@@ -74,6 +85,9 @@ temp=$(aws lambda create-function --function-name reset-func \
 resetFunctionArn=$(aws lambda get-function-configuration --function-name reset-func --region $REGION --query "FunctionArn" --output text)
 echo "Your reset function is created!"
 rm reset-func.py tempzip.zip
+
+
+
 
 #echo 'Creating Events...'
 
@@ -94,3 +108,22 @@ aws lambda add-permission \
 --principal events.amazonaws.com \
 --source-arn $default_stop_rule_arn
 
+
+
+#copied from run.sh
+days=( [0]="MON" [1]="TUE" [2]="WED" [3]="THUR" [4]="FRI" [5]="SAT" [6]="SUN")
+for ((i=0;i<7;i++))
+do
+	echo 'Enter Start Time..'
+	read starttime
+	echo 'Enter End Time'
+	read endtime
+
+	aws events put-rule \
+	--name ${days[i]}-start-rule \
+	--schedule-expression "cron(0 ${starttime} ? * ${days[i]} *)"
+
+	aws events put-rule \
+	--name ${days[i]}-stop-rule \
+	--schedule-expression "cron(0 ${endtime} ? * ${days[i]} *)"
+done
